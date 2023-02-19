487682a1a0eb1a87c3efb294c3679c8fa0259b72
mucrow <120820315+mucrow@users.noreply.github.com>
2023-02-19 16:24:03 -0500

    crude attack animation

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 namespace Mtd {
   public class Unit: MonoBehaviour {
+    [SerializeField] Sprite _idleSprite;
+    [SerializeField] Sprite _attackSprite1;
+    [SerializeField] Sprite _attackSprite2;
+
     [SerializeField] EnemyDetector _enemyDetector;
+    [SerializeField] SpriteRenderer _spriteRenderer;
 
     [SerializeField] int _damage = 10;
     [SerializeField] float _speed = 20.0f;
@@ -14,11 +19,17 @@ namespace Mtd {
 
     float _actionCooldownTimer = 0f;
 
+    float _attackFrame1Length = 5f / 60f;
+    float _attackFrame2Length = 15f / 60f;
+    float _animationTimer = 0f;
+
     void Awake() {
       _timeBetweenActions = CalculateTimeBetweenActions(_speed);
     }
 
     void Update() {
+      UpdateSprite();
+
       if (_actionCooldownTimer > 0f) {
         _actionCooldownTimer -= Time.deltaTime;
         return;
@@ -27,7 +38,30 @@ namespace Mtd {
       if (_enemyDetector.EnemiesInRange.Count > 0) {
         var enemy = _enemyDetector.EnemiesInRange[^1];
         enemy.ReceiveDamage(_damage);
+        StartAttackAnimation(enemy);
         _actionCooldownTimer += _timeBetweenActions;
+      }
+    }
+
+    void StartAttackAnimation(EnemyController target) {
+      _spriteRenderer.flipX = target.transform.position.x > transform.position.x;
+      _spriteRenderer.sprite = _attackSprite1;
+      _animationTimer = _attackFrame1Length;
+    }
+
+    void UpdateSprite() {
+      if (_spriteRenderer.sprite == _idleSprite) {
+        return;
+      }
+      _animationTimer -= Time.deltaTime;
+      if (_animationTimer <= 0f) {
+        if (_spriteRenderer.sprite == _attackSprite1) {
+          _spriteRenderer.sprite = _attackSprite2;
+          _animationTimer += _attackFrame2Length;
+        }
+        else if (_spriteRenderer.sprite == _attackSprite2) {
+          _spriteRenderer.sprite = _idleSprite;
+        }
       }
     }
 
