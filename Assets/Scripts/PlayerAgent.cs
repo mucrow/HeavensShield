3de2c86ed3d1c5993cf6c1554ba81d147fe3e566
mucrow <120820315+mucrow@users.noreply.github.com>
2023-02-19 18:32:28 -0500

    prototype unit creation UI

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -18,7 +18,6 @@ namespace Mtd {
     }
 
     void OnClick(InputValue value) {
-      Debug.Log("OnClick");
       if (value.isPressed) {
         // TODO figure out why value.isPressed is true twice on my (android) phone's touchscreen
         // left click press / touch start
