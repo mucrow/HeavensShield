9c045257025e37403c4b82de5e7e308c02a90fb6
mucrow <120820315+mucrow@users.noreply.github.com>
2023-02-25 11:44:22 -0500

    WIP convert Send Messages code to Unity Events (input)

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 namespace Mtd {
   public class PlayerAgent: MonoBehaviour {
@@ -17,24 +18,51 @@ namespace Mtd {
       }
     }
 
-    void OnClick(InputValue value) {
-      if (value.isPressed) {
-        // TODO figure out why value.isPressed is true twice on my (android) phone's touchscreen
-        // left click press / touch start
-        if (!_itemBeingPlaced) {
-          _itemBeingPlaced = Instantiate(_coffeeMugPrefab, _pointPosition, Quaternion.identity);
-        }
-      }
-      else {
-        // left click release / touch end
-        _itemBeingPlaced = null;
-      }
+    public void OnClick(InputAction.CallbackContext context) {
+      Debug.Log(context.phase);
     }
 
-    void OnPoint(InputValue value) {
-      var screenPos = value.Get<Vector2>();
-      _pointPosition = Camera.main.ScreenToWorldPoint(screenPos);
-      _pointPosition.z = 0;
-    }
+    // void OnClick(InputValue value) {
+    //   if (value.isPressed) {
+    //     // TODO figure out why value.isPressed is true twice on my (android) phone's touchscreen
+    //     // left click press / touch start
+    //     // Debug.Log("OnClick isPressed=true");
+    //     if (!_itemBeingPlaced) {
+    //       _itemBeingPlaced = Instantiate(_coffeeMugPrefab, _pointPosition, Quaternion.identity);
+    //     }
+    //   }
+    //   else {
+    //     // Debug.Log("OnClick isPressed=false");
+    //     // left click release / touch end
+    //     _itemBeingPlaced = null;
+    //   }
+    // }
+    //
+    // void OnClick2(InputValue value) {
+    //   try {
+    //     var touch = value.Get<Touch>();
+    //     Debug.Log("Able to Get<Touch>(): " + touch);
+    //   }
+    //   catch {}
+    //
+    //   try {
+    //     var obj = value.Get();
+    //     Debug.Log(obj);
+    //   }
+    //   catch {}
+    //
+    //   if (value.isPressed) {
+    //     Debug.Log("OnClick2 isPressed=true");
+    //   }
+    //   else {
+    //     Debug.Log("OnClick2 isPressed=false");
+    //   }
+    // }
+    //
+    // void OnPoint(InputValue value) {
+    //   var screenPos = value.Get<Vector2>();
+    //   _pointPosition = Camera.main.ScreenToWorldPoint(screenPos);
+    //   _pointPosition.z = 0;
+    // }
   }
 }
