75b29e4fd1ffb625956a1c8bbccc13fa3abf4c97
mucrow <120820315+mucrow@users.noreply.github.com>
2023-02-28 20:24:11 -0500

    prevent clicking behind UI

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.UI;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 namespace Mtd {
-  public class PlayerAgent: MonoBehaviour {
+  public class PlayerAgent: MonoBehaviour, MtdInputActions.IMainActions {
     [SerializeField] GameObject _coffeeMugPrefab;
+    [SerializeField] EventSystem _eventSystem;
+    [SerializeField] GraphicRaycaster _uiGraphicRaycaster;
 
     Camera _camera;
+    MtdInputActions _mtdInputActions;
 
-    Vector3 _pointPosition;
+    PointEventPosition _pointEventPosition;
     GameObject _itemBeingPlaced;
+
+    void Awake() {
+      _mtdInputActions = new MtdInputActions();
+    }
+
+    void OnEnable() {
+      _mtdInputActions.Main.SetCallbacks(this);
+      _mtdInputActions.Main.Enable();
+    }
 
     void Start() {
       _camera = Camera.main;
@@ -20,7 +35,7 @@ namespace Mtd {
 
     void Update() {
       if (_itemBeingPlaced != null) {
-        _itemBeingPlaced.transform.position = _pointPosition;
+        _itemBeingPlaced.transform.position = _pointEventPosition.World;
       }
     }
 
@@ -29,7 +44,9 @@ namespace Mtd {
       // (there is a third call when the touch ends as well)
       if (context.action.IsPressed()) {
         if (!_itemBeingPlaced) {
-          _itemBeingPlaced = Instantiate(_coffeeMugPrefab, _pointPosition, Quaternion.identity);
+          if (!DidClickHitUI()) {
+            _itemBeingPlaced = Instantiate(_coffeeMugPrefab, _pointEventPosition.World, Quaternion.identity);
+          }
         }
       }
       else {
@@ -38,9 +55,29 @@ namespace Mtd {
     }
 
     public void OnPoint(InputAction.CallbackContext context) {
-      var screenPos = context.ReadValue<Vector2>();
-      _pointPosition = _camera.ScreenToWorldPoint(screenPos);
-      _pointPosition.z = 0;
+      if (context.phase == InputActionPhase.Performed) {
+        UpdatePointEventPosition(context.ReadValue<Vector2>());
+      }
+    }
+
+    // is this really the right way to do this?
+    bool DidClickHitUI() {
+      var pointerEventData = new PointerEventData(_eventSystem);
+      pointerEventData.position = _pointEventPosition.Screen;
+      List<RaycastResult> results = new List<RaycastResult>();
+      _uiGraphicRaycaster.Raycast(pointerEventData, results);
+      return results.Count > 0;
+    }
+
+    void UpdatePointEventPosition(Vector2 screenPosition) {
+      _pointEventPosition.Screen = screenPosition;
+      _pointEventPosition.World = _camera.ScreenToWorldPoint(screenPosition);
+      _pointEventPosition.World.z = 0f;
+    }
+
+    struct PointEventPosition {
+      public Vector2 Screen;
+      public Vector3 World;
     }
   }
 }
