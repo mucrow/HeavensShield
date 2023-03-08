07cb2e95883d866e5cb066f8617105038d68dce9
mucrow <120820315+mucrow@users.noreply.github.com>
2023-03-08 11:30:08 -0500

    Create ShowHideOffscreen component

---
+++ MobileTowerDefense/Assets/Scripts/UI/ShowHideOffscreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+namespace Mtd.UI {
+  [RequireComponent(typeof(RectTransform))]
+  public class ShowHideOffscreen: MonoBehaviour {
+    RectTransform _rectTransform;
+    Vector2 _initialPosition;
+    float _size = 100;
+
+    public bool IsHidden { get; private set; }
+
+    void Awake() {
+      _rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Start() {
+      // trying to check initial position and width in Awake() result in inaccurate values. i
+      // believe this is because Unity UI components are not guaranteed to be finished resizing
+      // when Awake() is called. it makes sense - the layout needs to be dynamically calculated and
+      // it has to happen somewhere. probably best that these objects don't get special treatment
+      // just for being part of Unity's main library.
+      _initialPosition = _rectTransform.anchoredPosition;
+      _size = _rectTransform.rect.width;
+      Hide();
+    }
+
+    public void Show() {
+      _rectTransform.anchoredPosition = _initialPosition;
+      IsHidden = false;
+    }
+
+    public void Hide() {
+      _rectTransform.anchoredPosition = _initialPosition + Vector2.left * _size;
+      IsHidden = true;
+    }
+  }
+}
--- MobileTowerDefense/Assets/Scripts/UI/UnitSelector.cs
+++ MobileTowerDefense/Assets/Scripts/UI/UnitSelector.cs
@@ -5,47 +5,20 @@ using UnityEngine;
 
 namespace Mtd.UI {
   public class UnitSelector: MonoBehaviour {
-    RectTransform _rectTransform;
+    [SerializeField] ShowHideOffscreen _showHideOffscreen;
 
-    Vector2 _initialPosition = Vector2.zero;
-    public bool IsHidden { get; private set; } = false;
-    float _width = 100f;
-
-    void Awake() {
-      _rectTransform = GetComponent<RectTransform>();
-    }
-
-    void Start() {
-      // trying to check initial position and width in Awake() result in inaccurate values. i
-      // believe this is because Unity UI components are not guaranteed to be finished resizing
-      // when Awake() is called. it makes sense - the layout needs to be dynamically calculated and
-      // it has to happen somewhere. probably best that these objects don't get special treatment
-      // just for being part of Unity's main library.
-      _initialPosition = _rectTransform.anchoredPosition;
-      _width = _rectTransform.rect.width;
-      Hide();
-    }
+    public bool IsHidden => _showHideOffscreen.IsHidden;
 
     public void StartUnitSelection(Vector3 placementPosition) {
       Globals.UI.SelectionCircle.Show();
       Globals.UI.SelectionCircle.SetWorldPosition(placementPosition);
       Globals.UI.SelectionCircle.StopPreviewingRange();
-      Show();
+      _showHideOffscreen.Show();
     }
 
     public void CancelUnitSelection() {
       Globals.UI.SelectionCircle.Hide();
-      Hide();
-    }
-
-    void Show() {
-      _rectTransform.anchoredPosition = _initialPosition;
-      IsHidden = false;
-    }
-
-    void Hide() {
-      _rectTransform.anchoredPosition = _initialPosition + Vector2.left * _width;
-      IsHidden = true;
+      _showHideOffscreen.Hide();
     }
   }
 }
