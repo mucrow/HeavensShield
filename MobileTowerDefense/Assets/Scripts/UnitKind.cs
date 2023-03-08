a2721d3669f0dae0e5a7b8366eac21e0d2f35436
mucrow <120820315+mucrow@users.noreply.github.com>
2023-03-08 12:54:05 -0500

    Create UnitKind scriptable object & hologram prefab family

---
+++ MobileTowerDefense/Assets/Scripts/UnitKind.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Mtd {
+  [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/UnitKind", order = 1)]
+  public class UnitKind: ScriptableObject {
+    [SerializeField] string _name;
+    public string Name => _name;
+
+    [SerializeField] int _placementCost;
+    public int PlacementCost => _placementCost;
+
+    [SerializeField] GameObject _prefab;
+    public GameObject Prefab => _prefab;
+
+    [SerializeField] GameObject _hologramPrefab;
+    public GameObject HologramPrefab => _hologramPrefab;
+  }
+}
