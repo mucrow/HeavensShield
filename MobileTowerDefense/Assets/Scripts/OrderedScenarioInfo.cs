93877ece2737a7a775e2d9f4a9a2c3e84c6d678f
mucrow <120820315+mucrow@users.noreply.github.com>
2023-04-01 15:13:49 -0400

    Generate scenario IDs and unlock lists automatically in ScenarioOrder

---
+++ MobileTowerDefense/Assets/Scripts/OrderedScenarioInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Mtd {
+  public class OrderedScenarioInfo {
+    public int ID;
+    public string Name;
+    public string Path;
+    public List<int> Unlocks;
+  }
+}
--- MobileTowerDefense/Assets/Scripts/ScenarioOrder.cs
+++ MobileTowerDefense/Assets/Scripts/ScenarioOrder.cs
@@ -1,9 +1,68 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Mtd {
   [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ScenarioOrder", order = 1)]
   public class ScenarioOrder: ScriptableObject {
     [SerializeField] ScenarioOrderChapter[] _chapters;
     public ScenarioOrderChapter[] Chapters => _chapters;
+
+    List<OrderedScenarioInfo> _indexedOrdering = null;
+
+    public OrderedScenarioInfo GetScenarioByID(int id) {
+      IndexOrdering();
+      return _indexedOrdering[id];
+    }
+
+    public List<OrderedScenarioInfo> GetScenariosByIDs(List<int> ids) {
+      IndexOrdering();
+      var ret = new List<OrderedScenarioInfo>(ids.Count);
+      foreach (int id in ids) {
+        ret.Add(_indexedOrdering[id]);
+      }
+      return ret;
+    }
+
+    public List<OrderedScenarioInfo> GetIndexedOrdering() {
+      IndexOrdering();
+      return _indexedOrdering;
+    }
+
+    public void IndexOrdering() {
+      if (_indexedOrdering != null) {
+        return;
+      }
+
+      // set this to the instance variable at the end so we don't corrupt the state of this object
+      var indexedOrdering = new List<OrderedScenarioInfo>();
+
+      int idOfFirstScenarioInChapter = 0;
+      for (int indexOfChapter = 0; indexOfChapter < _chapters.Length; ++indexOfChapter) {
+        var chapter = _chapters[indexOfChapter];
+        bool isLastChapter = indexOfChapter >= (_chapters.Length - 1);
+        var scenarios = chapter.Scenarios;
+        for (int indexOfScenario = 0; indexOfScenario < scenarios.Length; ++indexOfScenario) {
+          var scenario = scenarios[indexOfScenario];
+          bool isLastScenarioInChapter = indexOfScenario >= (scenarios.Length - 1);
+          bool isFinalScenario = isLastChapter && isLastScenarioInChapter;
+          var id = indexOfScenario + idOfFirstScenarioInChapter;
+          var unlocks = new List<int>();
+          if (!isFinalScenario) {
+            unlocks.Add(id + 1);
+          }
+          var orderedScenarioInfo = new OrderedScenarioInfo() {
+            ID = id,
+            Name = scenario.Name,
+            Path = scenario.Path,
+            Unlocks = unlocks
+          };
+          indexedOrdering.Add(orderedScenarioInfo);
+        }
+      }
+
+      // the ordering was indexed successfully
+      _indexedOrdering = indexedOrdering;
+    }
   }
 }
