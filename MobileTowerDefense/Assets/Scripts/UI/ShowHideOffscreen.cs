f43f51d61bee71d614ba3f42103d500520e33770
mucrow <120820315+mucrow@users.noreply.github.com>
2023-04-25 17:27:45 -0400

    Add "Fade" HideMode to ShowHideOffscreen

--- MobileTowerDefense/Assets/Scripts/UI/ShowHideOffscreen.cs
+++ MobileTowerDefense/Assets/Scripts/UI/ShowHideOffscreen.cs
@@ -3,11 +3,15 @@ using System.Threading.Tasks;
 using Mtd.Utils;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 namespace Mtd.UI {
   [RequireComponent(typeof(RectTransform))]
   public class ShowHideOffscreen: UIBehaviour, IEnsureReady {
-    [SerializeField] Direction _edge;
+    [FormerlySerializedAs("_edge")] [SerializeField] HideMode _hideMode;
+
+    [Header("Can be null unless Hide Mode is \"Fade\".")]
+    [SerializeField] CanvasGroup _canvasGroup;
 
     [SerializeField] float _tweenTime = 0.1f;
 
@@ -20,6 +24,12 @@ namespace Mtd.UI {
 
     protected override void Awake() {
       _rectTransform = GetComponent<RectTransform>();
+
+      if (_hideMode == HideMode.Fade && _canvasGroup == null) {
+        Debug.LogError("You must set the Canvas Group property when Hide Mode is \"Fade\".", this);
+        Debug.LogError("Changing Hide Mode to \"Slide Down\".", this);
+        _hideMode = HideMode.SlideDown;
+      }
     }
 
     protected override void Start() {
@@ -46,29 +56,59 @@ namespace Mtd.UI {
     }
 
     public void ShowInstant() {
-      _rectTransform.anchoredPosition = _initialPosition;
+      if (_hideMode == HideMode.Fade) {
+        SetCanvasAlpha(1f);
+      }
+      else {
+        _rectTransform.anchoredPosition = _initialPosition;
+      }
       IsHidden = false;
     }
 
     public void HideInstant() {
-      var hiddenPosition = GetHiddenPosition();
-      _rectTransform.anchoredPosition = hiddenPosition;
+      if (_hideMode == HideMode.Fade) {
+        SetCanvasAlpha(0f);
+      }
+      else {
+        var hiddenPosition = GetHiddenPosition();
+        _rectTransform.anchoredPosition = hiddenPosition;
+      }
       IsHidden = true;
     }
 
     public Task Show() {
       var tcs = new TaskCompletionSource<bool>();
-      LeanTween.move(_rectTransform, _initialPosition, _tweenTime).setOnComplete(() => {
+
+      LTDescr tween;
+      if (_hideMode == HideMode.Fade) {
+        tween = TweenCanvasAlpha(1f, _tweenTime);
+      }
+      else {
+        var hiddenPosition = GetHiddenPosition();
+        tween = LeanTween.move(_rectTransform, _initialPosition, _tweenTime);
+      }
+
+      tween.setOnComplete(() => {
         tcs.SetResult(true);
       }).setIgnoreTimeScale(true);
+
       IsHidden = false;
       return tcs.Task;
     }
 
     public Task Hide() {
       var tcs = new TaskCompletionSource<bool>();
-      var hiddenPosition = GetHiddenPosition();
-      LeanTween.move(_rectTransform, hiddenPosition, _tweenTime).setOnComplete(() => {
+
+      LTDescr tween;
+      if (_hideMode == HideMode.Fade) {
+        tween = TweenCanvasAlpha(0f, _tweenTime);
+      }
+      else {
+        var hiddenPosition = GetHiddenPosition();
+        tween = LeanTween.move(_rectTransform, hiddenPosition, _tweenTime);
+      }
+
+      tween.setOnComplete(() => {
         tcs.SetResult(true);
       }).setIgnoreTimeScale(true);
       IsHidden = true;
@@ -76,13 +116,16 @@ namespace Mtd.UI {
     }
 
     Vector2 GetHiddenPosition() {
-      var direction = _edge.ToVector2();
+      var direction = _hideMode.ToVector2();
       var relevantDimension = GetRelevantDimension();
       return _initialPosition + direction * relevantDimension;
     }
 
     public float GetRelevantDimension() {
-      if (_edge == Direction.Left || _edge == Direction.Right) {
+      if (_hideMode == HideMode.Fade) {
+        return 0f;
+      }
+      if (_hideMode == HideMode.SlideLeft || _hideMode == HideMode.SlideRight) {
         return _size.x;
       }
       return _size.y;
@@ -96,6 +139,33 @@ namespace Mtd.UI {
       _size = _rectTransform.rect.size;
       HideInstant();
       _isReady = true;
+    }
+
+    LTDescr TweenCanvasAlpha(float alpha, float time) {
+      return ChangeCanvasAlphaHelper(alpha, true, time);
+    }
+
+    void SetCanvasAlpha(float alpha) {
+      ChangeCanvasAlphaHelper(alpha, false, 0f);
+    }
+
+    /**
+     * A helper method that helps me avoid forgetting to update _canvasGroup.blocksRaycasts. Also
+     * ensures I don't forget to make the tween use the "ease out circular" curve.
+     *
+     * If alpha is 0, _canvasGroup.blocksRaycasts is set to false.
+     * Otherwise, _canvasGroup.blocksRaycasts is set to true.
+     *
+     * If time is greater than 0, an LTDescr is returned.
+     * Otherwise, null is returned.
+     */
+    LTDescr ChangeCanvasAlphaHelper(float alpha, bool returnLTDescr, float time) {
+      _canvasGroup.blocksRaycasts = alpha != 0;
+      if (returnLTDescr) {
+        return LeanTween.alphaCanvas(_canvasGroup, alpha, time).setEaseOutCirc();
+      }
+      _canvasGroup.alpha = alpha;
+      return null;
     }
   }
 }
---
+++ MobileTowerDefense/Assets/Scripts/Utils/HideMode.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Mtd.Utils {
+  public enum HideMode {
+    Fade = 4,
+    SlideLeft = 0,
+    SlideRight = 1,
+    SlideDown = 2,
+    SlideUp = 3,
+  }
+
+  public static class HideModeExtensions {
+    public static bool IsDirectional(this HideMode hideMode) {
+      return hideMode != HideMode.Fade;
+    }
+
+    public static Vector2 ToVector2(this HideMode hideMode) {
+      if (hideMode == HideMode.SlideLeft) {
+        return Vector2.left;
+      }
+      if (hideMode == HideMode.SlideRight) {
+        return Vector2.right;
+      }
+      if (hideMode == HideMode.SlideDown) {
+        return Vector2.down;
+      }
+      if (hideMode == HideMode.SlideUp) {
+        return Vector2.up;
+      }
+      return Vector2.zero;
+    }
+  }
+}
