8b895eed8c5c281918987c1cb7656c1103d0f3a1
mucrow <120820315+mucrow@users.noreply.github.com>
2023-05-26 14:45:57 -0400

    Make MapEdgeOverlay sprite invisible in editor

--- MobileTowerDefense/Assets/Scripts/Fog.cs
+++ MobileTowerDefense/Assets/Scripts/Fog.cs
@@ -5,18 +5,24 @@ namespace Mtd {
   public class Fog: MonoBehaviour {
     [SerializeField] Tilemap _tilemap;
 
-    [SerializeField] GameObject _westEdge;
-    [SerializeField] GameObject _eastEdge;
-    [SerializeField] GameObject _northEdge;
-    [SerializeField] GameObject _southEdge;
+    [SerializeField] SpriteRenderer _westEdge;
+    [SerializeField] SpriteRenderer _eastEdge;
+    [SerializeField] SpriteRenderer _northEdge;
+    [SerializeField] SpriteRenderer _southEdge;
 
     void Start() {
       _tilemap.CompressBounds();
       var mapBounds = _tilemap.localBounds;
+
       _westEdge.transform.position = new Vector3(mapBounds.min.x - _westEdge.transform.localScale.x / 2f, mapBounds.center.y, 0f);
       _eastEdge.transform.position = new Vector3(mapBounds.max.x + _eastEdge.transform.localScale.x / 2f, mapBounds.center.y, 0f);
       _southEdge.transform.position = new Vector3(mapBounds.center.x, mapBounds.min.y - _southEdge.transform.localScale.y / 2f, 0f);
       _northEdge.transform.position = new Vector3(mapBounds.center.x, mapBounds.max.y + _northEdge.transform.localScale.y / 2f, 0f);
+
+      _westEdge.enabled = true;
+      _eastEdge.enabled = true;
+      _southEdge.enabled = true;
+      _northEdge.enabled = true;
     }
   }
 }
