6f304dac39dbc7e0f4fa287ba91a1dd6795ef1ba
mucrow <120820315+mucrow@users.noreply.github.com>
2023-06-13 17:19:36 -0400

    Add OnUIReady callback to SimpleUISceneManager

--- MobileTowerDefense/Assets/Scripts/SimpleSceneUIManager.cs
+++ MobileTowerDefense/Assets/Scripts/SimpleSceneUIManager.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using Mtd.UI;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Mtd {
   public class SimpleUISceneManager: MonoBehaviour {
     [SerializeField] AudioClip _music;
     [SerializeField] UIObjectKey[] _uiObjectKeys;
 
+    [SerializeField] UnityEvent _onUIReady;
+
     void Start() {
       Globals.AudioManager.PlayMusic(_music);
       Globals.UI.EnsureReady();
+      _onUIReady.Invoke();
       foreach (UIObjectKey uiObjectKey in _uiObjectKeys) {
         var component = Globals.UI.GetObjectByKey(uiObjectKey);
         component.ShowInstant();
